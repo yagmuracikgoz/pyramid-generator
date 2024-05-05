e7ad75997972a3459549d76926b909e89708e0fd
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-05 16:25:56 +0300

    step 38

---
+++ index.js
@@ -0,0 +1,14 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
+for (const row of rows) {
+    result = result +"\n"+ row;
+}
+
+console.log(result);
