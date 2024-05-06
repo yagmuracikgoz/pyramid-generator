8b745bbcd530dca368a623975bce405a7efaaddb
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-06 13:54:37 +0300

    final i think

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i ++) {
-  rows.push(padRow(i + 1, count));
+
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
 }
+
+while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}
+
 
 let result = ""
 
