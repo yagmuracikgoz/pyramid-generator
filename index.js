20610446dac352164c334807e881c20c8a3e65d3
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-06 14:13:33 +0300

    final work

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-
 for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
-
-while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}
-
 
 let result = ""
 
